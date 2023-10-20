f22aa28d6cd275417873c43f78c7913824bd0ee3
nsbsb99 <solbin9910@gmail.com>
2023-10-20 17:54:47 +0900

    <Solbin> feat: 실타격

--- TowerDefence/Assets/_Project Main/Scripts/Unit/Bullet/Bullet_HitBoss.cs
+++ TowerDefence/Assets/_Project Main/Scripts/Unit/Bullet/Bullet_HitBoss.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class Bullet_HitBoss : MonoBehaviour
 {
     FinalBoss finalBoss = new FinalBoss();
+    public int damage = default; // UnitAttack_ShootBoss에서 전달받는 데미지 계수
+    private Vector3 poolPos = new Vector3(0, -10, 0); // 풀 포지션
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision) // TODO: 데미지를 한 번만 입히도록
     {
         if (collision.gameObject.CompareTag("Boss"))
         {
-            Debug.Log("보스 타격!");
+            Debug.Log("보스에게 데미지 발생!");
 
             GameObject finalBoss = collision.gameObject;
-            finalBoss.GetComponent<FinalBoss>().HitDamage(10); // TODO: 데미지 임시값 교체 
+            finalBoss.GetComponent<FinalBoss>().HitDamage(damage); // 보스에 데미지를 입힘. 
+            
+            ReturnPool();
         }
 
-        // TODO: (임시) Destroy, (후에 수정) 오브젝트 풀링
     }
+
+    private void ReturnPool() { transform.position = poolPos; } // 풀로 복귀
 }
